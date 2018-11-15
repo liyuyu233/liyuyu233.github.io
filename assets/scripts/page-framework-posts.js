c6c05597443493c387a0ebc8e437654f743caeb1
liyuyu233 <43329101+liyuyu233@users.noreply.github.com>
2018-11-16 02:40:20 +0800

    test

--- assets/scripts/page-framework-posts.js
+++ assets/scripts/page-framework-posts.js
@@ -1,11 +1,6 @@
 // update_sidebar_view
 var height_window = window.innerHeight;
-var css_main = document.getElementById("css_page_framework");
-// adjust the height of #sidebar to parent height - padding
-css_main.sheet.insertRule(
-	"#sidebar {\
-		min-height:" + (height_window - 100) + "px;\
-	}");
+var css_main = document.getElementById("css_page_framework_posts");
 
 // adjust the height of #content_container
 var element_content_container = document.getElementById("content_container");
@@ -13,11 +8,4 @@ var height_content_container = element_content_container.clientHeight;
 css_main.sheet.insertRule(
 	"#content_container {\
 		min-height:px;\
-	}");
-// insert at rule that hides sidebar_character
-css_main.sheet.insertRule(
-	"@media screen and (max-height:px) {\
-		#sidebar_character {\
-			visibility: hidden;\
-		}\
 	}");
